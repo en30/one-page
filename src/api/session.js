ead883ae7aae7e4abc6bfe07432a72999eb5681e
en30 <en30.git@gmail.com>
2018-03-20 00:04:29 +0900

    moves firebase initialization to index.js

--- src/api/session.js
+++ src/api/session.js
@@ -1,22 +1,9 @@
 import firebase from "firebase";
-import user from "./user";
-
-const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: "",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
-};
-
-firebase.initializeApp(config);
-
-const provider = new firebase.auth.TwitterAuthProvider();
-firebase.auth().languageCode = "ja";
 
 const create = async function() {
   try {
+    firebase.auth().languageCode = "ja";
+    const provider = new firebase.auth.TwitterAuthProvider();
     await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
     const result = await firebase.auth().signInWithPopup(provider);
     return result.user;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import firebase from "firebase";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: "",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
